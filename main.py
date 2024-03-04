8269f850c6e52c7feb85d174e69a2c36035fb6dc
foroozan shafiei <f.shafiei@Hamgamco.org>
2024-03-04 14:32:21 +0330

    Start

--- main.py
+++ main.py
@@ -1,16 +1,79 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_board(board):
+    for row in board:
+        print(" | ".join([cell if cell is not None else " " for cell in row]))
+        print("-" * 5)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def check_winner(board, player):
+    for row in board:
+        if all([cell == player for cell in row]):
+            return True
+    for col in range(3):
+        if all([board[row][col] == player for row in range(3)]):
+            return True
+    if all([board[i][i] == player for i in range(3)]) or all([board[i][2 - i] == player for i in range(3)]):
+        return True
+    return False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def is_board_full(board):
+    return all([cell is not None for row in board for cell in row])
+
+
+def get_empty_cells(board):
+    return [(row, col) for row in range(3) for col in range(3) if board[row][col] is None]
+
+
+def player_move(board, player, start):
+    print_board(board)
+    empty_cells = get_empty_cells(board)
+    if not is_board_full(board):
+        if start == "c":
+            if player == "X":
+                choice = int(input(f"It's your turn, select an empty cell (1-9): "))
+                row, col = divmod(choice - 1, 3)
+            else:
+                print("Computer turn.")
+                choice = random.choice(empty_cells)
+                row, col = choice
+        else:
+            if player == "X":
+                choice = int(input(f"It's your turn, select an empty cell (1-9): "))
+                row, col = divmod(choice - 1, 3)
+            else:
+                choice = int(input(f"It's your friend's turn, select an empty cell (1-9): "))
+                row, col = divmod(choice - 1, 3)
+
+        if board[row][col] is None:
+            board[row][col] = player
+        else:
+            print("This cell is already in use.")
+            player_move(board, player, start)
+    return check_winner(board, player)
+
+
+def play_game():
+    board = [[None for _ in range(3)] for _ in range(3)]
+    players = ["X", "O"]
+    random.shuffle(players)
+    winner = None
+    print('Welcome to Tic Tac Toe game. You are X.')
+    start = input("Choose who you want to play with? Type 'c' for computer or 'p' for a person: ")
+
+    while not winner and not is_board_full(board):
+        for player in players:
+            if player_move(board, player, start):
+                winner = player
+                break
+
+    print_board(board)
+    if winner:
+        print(f"Player {winner} wins!")
+    else:
+        print("It's a tie!")
+
+
+play_game()
